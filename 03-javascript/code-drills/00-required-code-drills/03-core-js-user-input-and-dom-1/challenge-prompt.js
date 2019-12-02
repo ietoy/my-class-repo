6bfb80326283c1fc1799509faac39f5d132820e6
Ian Toy <iet@ela.com>
2019-12-01 16:04:28 -0800

    completed 03 JS code drills 3 and 4

--- 03-javascript/code-drills/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
+++ 03-javascript/code-drills/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
@@ -3,7 +3,8 @@
 
 
 // Declare a variable `userName`
-
+var userName;
+userName = prompt("What is your name?");
 
 // Prompt the user for their name and store their response into `userName`
 // hint: use javascript's built in `prompt` method
@@ -12,20 +13,26 @@
 
 // Create an alert welcoming the user to the "Sandwich Shop" using the name they just gave us
 
+alert("Welcome to the sandwich shop, " + userName +"!");
 
 
 // Using the `confirm` method, ask the user if they would like a sandwich
 // Store their response into a new `wantsSandwich` variable
 
+var wantsSandwich;
+wantsSandwich = confirm("Would you like a sandwich, " + userName + "?");
 
 
 // Check if the user wanted a sandwich
 
 
     // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
-
-    
+    if (wantsSandwich) {
+        document.write("Your sandwich will be ready soon!")
+    }
 // Else, say goodbye to the user using the name they gave us earlier
-
+    else {
+        document.write("Goodbye " + userName + "!")
+    }
 
 
--- 03-javascript/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ 03-javascript/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -5,23 +5,23 @@
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
 
+var wantsBoba;
+wantsBoba = confirm("Welcome to the Boba Shop! Would you like some boba?")
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
 
-
-
-    // Declare a variable `drinkFlavor`
-
-    
+if (wantsBoba) {
+    // Declare a variable `drinkFlavor`    
     // Prompt the user for what flavor they would like for their drink
     // Store their response into the variable `drinkFlavor`
-
-    
-    
+    var drinkFlavor;
+    drinkFlavor = prompt("What flavor would you like?");
     // Write to the DOM a message to the user telling them that their drink will be ready soon.
     // Use string concatenation in order to specify the flavor of their drink
-
-    
+    document.write("Your " + drinkFlavor + " boba will be ready soon!")
+}
 // Else, write "Goodbye! Come back soon!" to the DOM
-
+else {
+    document.write("Goodbye! Come back soon!")
+}
 
