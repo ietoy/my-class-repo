022c16a222bebadf0bdcde4c41ebb8d321d4e166
Ian Toy <iet@ela.com>
2019-12-02 19:52:30 -0800

    completed 03 JS code drill 8. I think I finally get functions now :)

--- 03-javascript/code-drills/00-required-code-drills/08-core-js-functions-1/challenge-prompt.js
+++ 03-javascript/code-drills/00-required-code-drills/08-core-js-functions-1/challenge-prompt.js
@@ -1,6 +1,8 @@
 // define a function "fun" that console logs "Functions are FUN!"
 
-
+function fun() {
+    console.log("Functions are FUN!");
+}
 
 // before moving on, open index.html and see what console logs have been made.
 // why doesn't "Functions are FUN! appear yet?"
@@ -8,65 +10,105 @@
 
 Your Answer here
 
+The console log doesn't appear because nothing calls the function
+
 */
 
 
 // define a function "thirtySeven" that console logs the sum of 18 and 19
 
+function thirtySeven() {
+    console.log(18 + 19);
+}
 
 
 // call two functions you've defined so far
 
+fun();
+
+thirtySeven();
 
 // define a function "log" that takes in an argument and console logs that argument
-
+function log(argument) {
+    console.log(argument);
+}
 
 
 // define a function "hello" that calls on the "log" function to console log "Hello World"
-
-
+function hello() {
+    log("Hello World!");
+};
 
 // call your "hello" function
-
+hello();
 
 // define a function "popUp" that creates an alert that says "This is an alert."
-
+function popUp() {
+    alert("THIS IS AN ALERT! YOU HAVE 12 SECONDS TO COMPLY!");
+};
 
 
 // define a function "noPopUp" that creates an alert that says "Okay, you won't get a pop up."
-
+function noPopUp() {
+    alert("Okay, you won't get a pop up.")
+};
 
 
 // define a function "goodDay" that creates a confirm dialogue asking the user if they are having a nice day
 // and uses your "log" function to display the response
+function goodDay() {
+    var response;
+    response = confirm("Are you having a good day?");
+    log(response);
+};
 
 
 
 // call your "goodDay" function
+goodDay();
 
 
 // define a function "userPopUp" that creates a confirm dialogue asking the user if they would like a popup
 // if they answer yes, call upon your "popUp" function; otherwise, call upon your "noPopUp" function
 
-
+function userPopUp() {
+    var response;
+    response = confirm("Would you like a pop up?");
+        if (response === true) {
+            popUp();
+        }
+        else {
+            noPopUp()
+        }
+};
 
 // call your "userPopUp" function
-
+userPopUp();
 
 // define a function "userName" that prompts the user for their name and then 
 // uses your "log" function to display the response
 
+function userName() {
+    var userName;
+    userName = prompt("What is your name?");
+    log(userName);
+};
 
 
 // call your "userName" function
-
+userName();
 
 // define a function "favoriteFruit" that prompts the user for their favorite fruit
 // and the creates an alert that displays the response
 
+function favoriteFruit() {
+    var response;
+    response = prompt("What is your favorite fruit?");
+    alert("Your favorite fruit is " + response);
+};
 
 
 // call your "favoriteFruit" function
-
+favoriteFruit();
 
 
